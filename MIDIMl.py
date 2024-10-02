1f85b9d39984da1db156b5959db031ee00bf07ac
anthonypaulbalducci <34378880+anthonypaulbalducci@users.noreply.github.com>
2024-10-01 20:43:46 -0400

    Update MIDIMl.py

--- MIDIMl.py
+++ MIDIMl.py
@@ -2,3 +2,11 @@ import tensorflow as tf
 import tensorflow_io as tfio
 
 # audio = tfio.audio.AudioIOTensor('gs://cloud-samples-tests/speech/brooklyn.flac')
+
+import tensorflow as tf
+
+dataset = tf.keras.utils.audio_dataset_from_directory(
+    "path/to/audio/directory",
+    batch_size=32,
+    labels='inferred'  # Infers labels from subdirectory names
+)
